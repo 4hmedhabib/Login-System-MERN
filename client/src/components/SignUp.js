5330dcd8824d12aa0f00aacecd4d059fc7e4ac0e
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-16 06:22:06 +0300

    added the sigup

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,5 +1,18 @@
 const SignUp = () => {
-	return <div>SignUp</div>;
+	return (
+		<form action="">
+			<div>
+				<label htmlFor="username">Username</label>
+				<br />
+				<input type="text" name="username" placeholader="Enter your username" id="username" />
+			</div>
+			<div>
+				<label htmlFor="password">Password</label>
+				<br />
+				<input type="text" name="password" placeholader="Enter your password" id="password" />
+			</div>
+		</form>
+	);
 };
 
 export default SignUp;
